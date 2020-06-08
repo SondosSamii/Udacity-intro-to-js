a25d734fb0b093d5da220c4eb70c472baa2a2bfe
SondosSamii <sondos.sami.abuzeid@gmail.com>
2020-06-08 02:02:36 +0200

    Inline Quiz

--- js/lesson5.js
+++ js/lesson5.js
@@ -57,3 +57,36 @@ var laugh = function (num) {
 }
 
 console.log(laugh(3));
+
+
+/*
+ * Programming Quiz: Inline Functions (5-6)
+ */
+
+/*
+* QUIZ REQUIREMENTS
+* - Your code should have an `emotions()` function
+* - Your code should call the `emotions()` function
+* - Your `emotions()` function call should have an inline function expression passed as the second parameter
+* - Your function expression should return the expected output
+*/
+
+
+// don't change this code
+// emotions() function definition
+function emotions(myString, myFunc) {
+    console.log("I am " + myString + ", " + myFunc(2));
+}
+
+// your code goes here
+// Call the emotions() function with two arguments
+// Argument 1 - "happy" string
+// Argument 2 - an inline function expression
+
+emotions("happy", function (num) {
+    var laughs = "";
+    for (var i = 0; i < num; i++) {
+        laughs += "ha";
+    }
+    return laughs + "!";
+});
