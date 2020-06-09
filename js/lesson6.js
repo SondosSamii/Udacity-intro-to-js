a1a818e1b7be4d5e7276a11c8eb4c42bf792c4d0
SondosSamii <sondos.sami.abuzeid@gmail.com>
2020-06-10 00:16:08 +0200

    Add hasEnoughPlayers() function

--- js/lesson6.js
+++ js/lesson6.js
@@ -56,3 +56,27 @@ var rainbow = ['Red', 'Orange', 'Blackberry', 'Blue'];
 rainbow.splice(2, 1, "Yellow", "Green");
 rainbow.splice(5, 0, "Purple");
 console.log(rainbow);
+
+
+/*
+ * Programming Quiz: Quidditch Cup (6-5)
+ */
+/*
+ * QUIZ REQUIREMENTS
+ * - Your code should have a function `hasEnoughPlayers()`
+ * - Your function `hasEnoughPlayers()` should accept one parameter
+ * - Your function `hasEnoughPlayers()` should return the expected output - a Boolean value - true or false
+ * - Return true if the array size is atleast 7, otherwise false. 
+ */
+
+// your code goes here
+
+var team = ["Oliver Wood", "Angelina Johnson", "Katie Bell", "Alicia Spinnet", "George Weasley", "Fred Weasley", "Harry Potter"];
+console.log(hasEnoughPlayers(team));
+
+function hasEnoughPlayers(team) {
+    if (team.length >= 7) {
+        return true;
+    }
+    return false;
+};
