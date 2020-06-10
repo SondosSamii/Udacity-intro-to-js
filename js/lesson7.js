6ecf14baa532433c9697450e0b639c39bfb5d6b4
SondosSamii <sondos.sami.abuzeid@gmail.com>
2020-06-11 00:38:26 +0200

    Add Facebook Friends and Messages Quiz

--- js/lesson7.js
+++ js/lesson7.js
@@ -89,3 +89,64 @@ var savingsAccount = {
 };
 
 console.log(savingsAccount.printAccountSummary());
+
+
+/*
+ * Programming Quiz: Facebook Friends (7-5)
+ */
+
+/*
+ * QUIZ REQUIREMENTS
+ * - Your code should have an object `facebookProfile`
+ * - The `facebookProfile` object should have the `name` (string), `friends` (number), and `messages` (array of strings) property
+ * - Your `facebookProfile` object should have the `postMessage()`, `deleteMessage()`, `addFriend()` and `removeFriend()` method
+ * - Carefully implement the desired functionality of each method, as decribed above
+ */
+
+
+// TIP - 
+// In an array, 
+// - addition at the end is done using push() method
+// - addition at a specific index is done using splice() method
+// - deletion from the beginning is done using pop() method
+// - deletion from a specific index is done using splice() method
+
+// your code goes here
+var facebookProfile = {
+    name: "Sondos Sami",
+    friends: 290,
+    messages: ["The first message", "The second message", "The third message"],
+    postMessage: function (message) {
+        facebookProfile.messages.push(message);
+    },
+    deleteMessage: function (index) {
+        if (facebookProfile.messages.length > 0) {
+            facebookProfile.messages.splice(index, 1);
+        }
+    },
+    addFriend: function () {
+        facebookProfile.friends++;
+    },
+    removeFriend: function () {
+        if (facebookProfile.friends > 0) {
+            facebookProfile.friends--;
+        }
+    }
+};
+
+console.log(facebookProfile);
+
+facebookProfile.postMessage("Hey");
+
+console.log(facebookProfile.messages);
+
+facebookProfile.deleteMessage(3);
+
+console.log(facebookProfile.messages);
+
+facebookProfile.addFriend();
+
+facebookProfile.addFriend();
+facebookProfile.addFriend();
+facebookProfile.removeFriend();
+console.log(facebookProfile.friends);
